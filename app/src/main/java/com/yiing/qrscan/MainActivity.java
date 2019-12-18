8943a8e8fa8eb2e352e1e79702e565519a3b4be0
ladarat <ladarat.y@gmail.com>
2019-12-18 12:23:35 +0700

    Scan qr

--- app/src/main/java/com/yiing/qrscan/MainActivity.java
+++ app/src/main/java/com/yiing/qrscan/MainActivity.java
@@ -1,13 +1,101 @@
 package com.yiing.qrscan;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.VisibleForTesting;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
-public class MainActivity extends AppCompatActivity {
+import android.Manifest;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    private ZXingScannerView mScannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.CAMERA},
+                1);
+
+        ViewGroup contentFrame = findViewById(R.id.content_frame);
+
+        mScannerView = new ZXingScannerView(this);
+        contentFrame.addView(mScannerView);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(final Result rawResult) {
+        scanQr(rawResult.getText());
+    }
+
+    @VisibleForTesting
+    public void scanQr(final String rawQr) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                final Dialog dialog = new Dialog(MainActivity.this);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setCancelable(true);
+                dialog.setContentView(R.layout.custom_dialog);
+
+                View v = Objects.requireNonNull(dialog.getWindow()).getDecorView();
+                v.setBackgroundResource(android.R.color.transparent);
+
+                TextView text = dialog.findViewById(R.id.someText);
+                text.setText(rawQr);
+                ImageView img = dialog.findViewById(R.id.imgOfDialog);
+                img.setImageResource(R.drawable.ic_done_gr);
+                dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+                        mScannerView.resumeCameraPreview(MainActivity.this);
+                    }
+                });
+                dialog.show();
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1) {
+            if (grantResults.length <= 0
+                    || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MainActivity.this, "Permission denied to camera", Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+        throw new IllegalStateException("Unexpected value: " + requestCode);
     }
 }
